9ddebd033e986a9c6ac1838be4c5eefb49d6c500
huunhatdev <nhatnh98@gmail.com>
2024-09-11 15:37:23 +0700

    fix add loader

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
 import "./assets/styles/index.css";
 import App from "./App.jsx";
+import { Loader } from "@react-three/drei";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+    <>
+        <App />
+        <Loader />
+    </>
+);
